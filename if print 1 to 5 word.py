13ab864146cd4f3079ecb45119cab5ff5c0f4951
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-11 17:33:45 +0530

    Create if print 1 to 5 word.py

---
+++ if print 1 to 5 word.py
@@ -0,0 +1,15 @@
+n=int(input("enter a number:"))
+if(n==1):
+ print("one")
+if(n==2):
+ print("two")
+if(n==3):
+ print("three")
+if(n==4):
+ print("four")
+if(n==5):
+ print("five")
+if(n<0 or n>5):
+ print("invalid number")
+ 
+ 
