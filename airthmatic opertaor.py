46ff4fc76faf0355ed1859a77e8bea3f488ecc94
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-11 10:41:05 +0530

    Create airthmatic opertaor.py

---
+++ airthmatic opertaor.py
@@ -0,0 +1,16 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+addition=a+b
+subtraction=a-b
+multiplaction=a*b
+divide=a/b
+modules=a%b   #module 
+floor_division=a//b  #floor divisoin
+power=a**b  #square root
+print("addtion:",addition)
+print("subtraction:",subtraction)
+print("multiplaction:",multiplaction)
+print("divide:",divide)
+print("modules:",modules)
+print("floor division:",floor_division)
+print("power:",power)
