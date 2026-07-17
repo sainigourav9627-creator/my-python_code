4fe7b0fbe876c44fbaa992b7313064c021f72b23
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-17 10:53:12 +0530

    Create three number sum or average.py

---
+++ three number sum or average.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first number: ")) 
+b=int(input("enter thes second number: "))
+c=int(input("enter the third number : "))
+
+sum = a + b + c
+average = sum / 3
+
+print("Sum:", sum)
+print("Average:", average)
