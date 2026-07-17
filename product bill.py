3d70e8c7604e4ba291ad4eb494edaf0c34cb7d28
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-17 10:46:17 +0530

    Create product bill.py

---
+++ product bill.py
@@ -0,0 +1,11 @@
+product=input("enter the product: ")
+price=int(input("enter the price: "))
+quantity=int(input("enter the quality: "))
+
+total=price*quantity
+
+print("\nProduct bill:",product)
+print("------------")
+print("Price:",price)
+print("quantity:",quantity)
+print("Total:",total)
