69cb06403ac6b33c27ab7ae0b9bd4f4ac4f507f7
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-18 16:38:03 +0530

    Create list indexing positive.py

---
+++ list indexing positive.py
@@ -0,0 +1,20 @@
+fruits = ["Apple", "Mango", "Banana", "Orange"]
+
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
+print(fruits[3])
+
+numbers = [10, 20, 30, 40]
+print(numbers[2])
+
+students = ["Amit", "Rahul", "Gourav", "Anil"]
+print(students[0])
+
+cities = ["Delhi", "Mumbai", "Lucknow"]
+print(cities[1])
+
+data = ["Python", "Java", "C", "HTML"]
+print(data[0])
+print(data[2])
+print(data[3])
