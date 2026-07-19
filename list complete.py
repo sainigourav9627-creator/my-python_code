6ed9927efa24edb15feb549daa926118c53df793
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-19 10:32:49 +0530

    Create list complete.py

---
+++ list complete.py
@@ -0,0 +1,28 @@
+# List
+name = [10,20,30,40,50]
+print(name)
+
+# Positive Indexing
+print(name[4])
+
+# Negative Indexing
+print(name[-2])
+
+# Slicing [start:end]
+print(name[0:4])
+
+# Step Slicing
+name = [10,20,30,40,50,60,70,80,90]
+print(name[0:8:2])
+
+# Step Slicing (Start omitted)
+print(name[::2])
+
+# Step Slicing (Start = 1)
+print(name[1::2])
+
+# Reverse List
+print(name[::-1])
+
+# Full Copy
+print(name[:])
