4ea0df80a963d106051fd0223426498f35fd4c98
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-19 14:59:27 +0530

    Create list insert method.py

---
+++ list insert method.py
@@ -0,0 +1,10 @@
+#insert() का उपयोग किसी भी Index (Position) पर नया Element जोड़ने के लिए किया जाता है।।
+num = [10,20,30,40]   #शुरुआत में Insert
+fruits=["mango","apple","bananna","grapes"]  
+num1 = [10,20,30,40]   
+num.insert(0,3)
+fruits.insert(2,"guava")
+num1.insert(4,88)
+print(num)
+print(fruits)
+print(num1)
