205c426cd27ebdc29943d032f8a5dafe66ecb94e
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-19 14:24:19 +0530

    Create list append().py

---
+++ list append().py
@@ -0,0 +1,11 @@
+#append() Method का उपयोग List के Last (अंत) में एक नया Element जोड़ने के लिए किया जाता है।
+name = [10,20,30,40,50,60,70,80,90]
+fruits=["mango","banana","apple"]
+fruits.append("graps")
+name.append(100)
+name.append(25.5)
+name.append(True)
+name.append("mango")
+name.append([10,50])
+print(name)
+print(fruits)
