67f8dff3bbaf49680f1fd1015c51fc276bb82c04
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-19 14:32:24 +0530

    Create list append() & extend method.py

---
+++ list append() & extend method.py
@@ -0,0 +1,8 @@
+# append() और extend() का सबसे बड़ा Difference है।
+name = [10,20,30,40]
+fruits=["mango","banana","apple"]
+fruits.append("graps")
+name.append([1,2])
+name.extend([100,50,2,3])
+print(name)
+print(fruits)
