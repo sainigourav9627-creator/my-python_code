a2017304b952febf1260cb942627140215445956
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-21 15:19:20 +0530

    Create (Kilometer Converter).py

---
+++ (Kilometer Converter).py
@@ -0,0 +1,7 @@
+Kilometer=float(input("enter the kilometer:"))
+meter= Kilometer * 1000
+centimeter= Kilometer * 100000
+millimeter= Kilometer * 1000000
+print(f"Meter = {meter}")
+print(f"Centimete = {centimeter}")
+print(f"Milimeter = {millimeter}")
