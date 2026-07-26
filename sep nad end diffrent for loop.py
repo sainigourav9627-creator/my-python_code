c6780d4407167f5ed3ab0dcf1ec6b88e20fc3a2b
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-26 14:53:37 +0530

    Create sep nad end diffrent for loop.py

---
+++ sep nad end diffrent for loop.py
@@ -0,0 +1,21 @@
+Yaad rakhne ki Trick
+print(i, j)
+
+Isme 2 cheezein hoti hain:
+
+sep = Do values ke beech kya aayega?
+end = Print ke baad kya aayega?
+Parameter	Kaam
+sep	Do values ke beech separator
+end	Print ke baad kya hoga
+
+
+Example
+print("A", "B", sep="-", end=" ")
+print("C")
+
+Output:
+
+A-B C
+sep="-" → A-B
+end=" " → B ke baad space
