fc1bace7bdd827eadeb5d7830d9fdf19a362be20
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-26 14:48:46 +0530

    Create print blank and print without blank for loop.py

---
+++ print blank and print without blank for loop.py
@@ -0,0 +1,17 @@
+print() ki wajah se har outer loop ke baad ek blank line aa jati hai.
+
+for i in range(1,4):
+    for j in range(1,3):
+        print(i, j)
+    print()
+
+
+Yahan blank line nahi aayegi.
+
+for i in range(1,4):
+    for j in range(1,3):
+        print(i, j)
+
+
+
+
