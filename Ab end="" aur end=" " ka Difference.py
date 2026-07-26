1a061e8e62a39ca5167c2ac0523eebec34db4045
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-26 14:22:40 +0530

    Create Ab end="" aur end=" " ka Difference.py

---
+++ Ab end="" aur end=" " ka Difference.py
@@ -0,0 +1,26 @@
+Ab end="" aur end=" " ka Difference
+1. end=""
+for i in range(3):
+    print("*", end="")
+
+Output:
+
+***
+
+👉 Stars ke beech me koi space nahi.
+
+2. end=" "
+for i in range(3):
+    print("*", end=" ")
+
+Output:
+
+* * *
+
+👉 Har star ke baad ek space aayegi.
+
+Yaad Rakhne ki Trick ⭐
+Code	Output
+end=""	***
+end=" "	* * *
+end="\n"	Har print ke baad next line
