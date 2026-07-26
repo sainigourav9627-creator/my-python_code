92dab9f07acd29785003c16ba40feba6e82c1efd
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-26 14:04:52 +0530

    Create 2 variables (i aur j) ki har possible pair nested for loop.py

---
+++ 2 variables (i aur j) ki har possible pair nested for loop.py
@@ -0,0 +1,21 @@
+i tab tak same rehta hai, jab tak inner loop poora khatam na ho.
+Jab inner loop khatam hota hai, tab i badalta hai aur j fir se 1 se shuru hota hai.
+
+i = 1
+    j = 1 → print(1,1)
+    j = 2 → print(1,2)
+    j = 3 → print(1,3)
+
+i = 2
+    j = 1 → print(2,1)
+    j = 2 → print(2,2)
+    j = 3 → print(2,3)
+
+i = 3
+    j = 1 → print(3,1)
+    j = 2 → print(3,2)
+    j = 3 → print(3,3)
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(i,j)
