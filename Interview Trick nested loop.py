cba5362db2632365fead0dd4fc730637196869eb
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-26 15:55:21 +0530

    Create Interview Trick nested loop.py

---
+++ Interview Trick nested loop.py
@@ -0,0 +1,9 @@
+Agar ye dikhe	Samajh jao
+range(1, i+1)	Triangle banega 📐
+range(1,5)	Square/Rectangle banega ⬜
+range(5,0,-1)	Reverse Pattern banega 🔻
+print("*")	Star Pattern ⭐
+print(i)	Same row number
+print(j)	1 se counting
+print(i,j)	Row-Column
+print(num)	Floyd's Triangle
