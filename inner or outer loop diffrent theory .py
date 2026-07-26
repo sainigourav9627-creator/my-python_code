e0906aaaa36a470ad7d11e348f3c4d0bc6439af7
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-26 14:42:55 +0530

    Create inner or outer loop diffrent theory .py

---
+++ inner or outer loop diffrent theory .py
@@ -0,0 +1,9 @@
+✅ i = Outer Loop (Rows)
+
+✅ j = Inner Loop (Columns)
+
+✅ print(i) → Outer loop ki value print hoti hai.
+
+✅ print(j) → Inner loop ki value print hoti hai.
+
+✅ print(i, j) → Dono ki current value print hoti hai.
