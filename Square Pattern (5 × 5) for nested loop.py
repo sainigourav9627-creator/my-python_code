ebf36efb75de4ac024eeedaff8492c031dbbc371
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-27 11:55:52 +0530

    Update Square Pattern (5 × 5) for nested loop.py

--- Square Pattern (5 × 5) for nested loop.py
+++ Square Pattern (5 × 5) for nested loop.py
@@ -2,4 +2,21 @@ for i in range(1,6):
     for j in range(1,6):
       print("*",end=" ")
     print()
-     
+
+i = 1
+    j = 1  *
+    j = 2  *
+    j = 3  *
+    j = 4  *
+    j = 5  *
+    ↓
+    Next line
+
+i = 2
+    j = 1  *
+    j = 2  *
+    j = 3  *
+    j = 4  *
+    j = 5  *
+    ↓
+    Next line
