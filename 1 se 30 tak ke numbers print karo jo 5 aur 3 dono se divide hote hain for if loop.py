00d9292e30a19421c9315936e66f4b93c2385f61
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-27 10:36:13 +0530

    Update 1 se 30 tak ke numbers print karo jo 5 aur 3 dono se divide hote hain for if loop.py

--- 1 se 30 tak ke numbers print karo jo 5 aur 3 dono se divide hote hain for if loop.py
+++ 1 se 30 tak ke numbers print karo jo 5 aur 3 dono se divide hote hain for if loop.py
@@ -1,3 +1,14 @@
+Important Rules
+
+    and
+➡️ Dono condition True honi chahiye.
+
+ or
+➡️ Koi ek condition True ho to bhi chalega.
+
+Ye interview mein bahut poochha jata hai.
+
+
 for i in range(1, 31):
     if i %5==0 and i%3==0:
               print(i)
