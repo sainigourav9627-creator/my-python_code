48e64988677adb6963f118b979ad5acabb1187ef
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-27 10:58:08 +0530

    Update 1 se 30 tak ke numbers print karo, lekin 5 ke multiples (5, 10, 15, 20, 25, 30) skip karo. for loop.py

--- 1 se 30 tak ke numbers print karo, lekin 5 ke multiples (5, 10, 15, 20, 25, 30) skip karo. for loop.py
+++ 1 se 30 tak ke numbers print karo, lekin 5 ke multiples (5, 10, 15, 20, 25, 30) skip karo. for loop.py
@@ -2,3 +2,10 @@ for i in range(1, 31):
     if i % 5 == 0:
         continue
     print(i)
+
+long method
+
+for i in range(1, 31):
+ if i == 5 or i == 10 or i == 15 or i == 20 or i == 25 or i == 30:
+        continue
+ print(i)
