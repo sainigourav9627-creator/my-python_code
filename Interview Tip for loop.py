62b5ce7acc1c5989c46a9525ac7d4103bf86ac73
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-27 10:38:07 +0530

    Create Interview Tip for loop.py with key conditions
    Added a Python script outlining four key conditions for loops.

---
+++ Interview Tip for loop.py
@@ -0,0 +1,10 @@
+
+
+Ye 4 conditions hamesha yaad rakhna:
+
+i % 2 == 0      # Even
+i % 2 != 0      # Odd
+i % n == 0      # n se divisible
+i % n != 0      # n se divisible nahi
+
+Inhi 4 concepts se Prime Number, Armstrong, Pattern, Factorial, FizzBuzz jaise programs bante hain.
