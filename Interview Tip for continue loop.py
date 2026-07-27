ecee5c0de55b8d47281c5557e8bc2c641088e377
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-27 10:51:21 +0530

    Create Interview Tip for continue loop.py

---
+++ Interview Tip for continue loop.py
@@ -0,0 +1,10 @@
+
+
+Ye dono conditions kab use karni hain, ye yaad rakhna:
+
+Condition	Meaning
+i == 5	Sirf 5
+i == 15	Sirf 15
+i % 5 == 0	5 ke saare multiples
+
+Ye difference interview mein bahut poochha jata hai.
