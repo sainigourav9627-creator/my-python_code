4c0214cef2bd3dd8dce68d019636ee8b60f851f7
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 11:57:24 +0530

    Create Right Aligned Increasing Star Pattern nested for loop.py

---
+++ Right Aligned Increasing Star Pattern nested for loop.py
@@ -0,0 +1,11 @@
+for i in range(1,5):
+    for k in range(1,6-i):
+       print(" ",end=" ")
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+        * 
+      * * 
+    * * * 
+  * * * * 
