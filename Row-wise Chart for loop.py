08221837f98dc2ed779f7660f152f322d3fb7037
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 16:38:19 +0530

    Create Row-wise Chart for loop.py

---
+++ Row-wise Chart for loop.py
@@ -0,0 +1,8 @@
+Row-wise Chart
+Row (i)	j kitni baar chalega	num ki values	Output
+
+i = 1	1 baar (j=1)	1	1
+i = 2	2 baar (j=1,2)	2, 3	2 3
+i = 3	3 baar (j=1,2,3)	4, 5, 6	4 5 6
+i = 4	4 baar (j=1,2,3,4)	7, 8, 9, 10	7 8 9 10
+i = 5	5 baar (j=1,2,3,4,5)	11, 12, 13, 14, 15	11 12 13 14 15
