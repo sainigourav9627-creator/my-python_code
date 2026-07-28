40adac8e7392c28f07971b42e5b987fe47e74250
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 14:19:48 +0530

    Update Half Pyramid Star Pattern nested for loop.py

--- Half Pyramid Star Pattern nested for loop.py
+++ Half Pyramid Star Pattern nested for loop.py
@@ -1,4 +1,4 @@
-for i in range(1,5):
+for i in range(1,6):
     for k in range(1,6-i):
        print("",end=" ")
     for j in range(1,i+1):
@@ -9,3 +9,4 @@ for i in range(1,5):
    * * 
   * * * 
  * * * * 
+* * * * * 
