aff03fd3406551bce7c21cefab355366f666997e
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 15:45:33 +0530

    Create Pattern Name List for loop.py

---
+++ Pattern Name List for loop.py
@@ -0,0 +1,17 @@
+Pattern Name List (Ab tak)
+No.	Pattern Name	Output Example
+
+1	Square Star Pattern	* * * *
+2	Row Number Pattern	1 2 3
+3	Column Number Pattern	1 1 1
+4	Left Increasing Half Pyramid	*, **, ***
+5	Left Decreasing Half Pyramid	*****, ****, ***
+6	Right Increasing Half Pyramid	Right aligned *
+7	Right Decreasing Half Pyramid	Right aligned decreasing *
+8	Full Pyramid	*, ***, *****
+9	Inverted Full Pyramid	*****, ***, *
+10	Diamond Pattern	Full + Inverted Pyramid
+11	Number Increasing Half Pyramid	1, 1 2, 1 2 3
+12	Number Decreasing Half Pyramid	1 2 3 4 5, 1 2 3 4
+13	Same Number Half Pyramid	1, 2 2, 3 3 3
+14	Reverse Number Half Pyramid ✅	1, 2 1, 3 2 1
