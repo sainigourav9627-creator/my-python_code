c83ddafb7ba56a41b2b1455ce0c644039cf24113
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 14:41:51 +0530

    Create Diamond Pattern ⭐ nested for loop.py

---
+++ Diamond Pattern ⭐ nested for loop.py
@@ -0,0 +1,23 @@
+for i in range(1,6):
+    for k in range(1,6-i):
+       print("",end=" ")
+    for j in range(1,2*i):
+        print("*",end=" ")
+    print()
+for i in range(4,0,-1):
+    for k in range(1,6-i):
+       print("",end=" ")
+    for j in range(1,2*i):
+        print("*",end=" ")
+    print()   
+
+
+    * 
+   * * * 
+  * * * * * 
+ * * * * * * * 
+* * * * * * * * * 
+ * * * * * * * 
+  * * * * * 
+   * * * 
+    * 
