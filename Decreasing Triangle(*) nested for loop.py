d10dc461ff5b347ac8f75bf7362bb2fde926e793
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 11:26:30 +0530

    Update Decreasing Triangle(*) nested for loop.py

--- Decreasing Triangle(*) nested for loop.py
+++ Decreasing Triangle(*) nested for loop.py
@@ -2,7 +2,13 @@ for i in range(5,0,-1):
     for j in range(1,i+1):
       print("*",end=" ")
     print()
+
+or
+
+for i in range(1,5):
+    for j in range(1,6-i):
+      print("*",end=" ")
+    print()
      
- 
 
    
