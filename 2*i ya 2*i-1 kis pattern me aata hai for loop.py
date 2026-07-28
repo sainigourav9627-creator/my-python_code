1dd40ac7d68178aa727d91178922e09867d759b3
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-28 14:46:46 +0530

    Create 2*i ya 2*i-1 kis pattern me aata hai for loop.py

---
+++ 2*i ya 2*i-1 kis pattern me aata hai for loop.py
@@ -0,0 +1,18 @@
+2*i ya 2*i-1 kis pattern me aata hai?
+Pattern	Formula
+✅ Square Star Pattern	i+1
+✅ Row Number Pattern	i / j
+✅ Column Number Pattern	i / j
+✅ Left Increasing Half Pyramid	i+1
+✅ Left Decreasing Half Pyramid	i+1
+✅ Right Increasing Half Pyramid	i+1
+✅ Right Decreasing Half Pyramid	i+1
+✅ Full Pyramid	2*i-1 (range(1, 2*i))
+✅ Inverted Full Pyramid	2*i-1 (range(1, 2*i))
+✅ Diamond Pattern	2*i-1 (Upper + Lower dono m
+
+
+Yaad rakhne ka shortcut:
+
+Half Pattern ➜ i+1
+Full Pattern ➜ 2*i (total stars = 2*i-1
