e54be03943be418702539ea86eaa145ab97c336c
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-29 17:45:11 +0530

    Create column matrix sum for loop.py

---
+++ column matrix sum for loop.py
@@ -0,0 +1,13 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for i in range(3):      # column
+    total = 0
+
+    for j in range(3):  # row
+        total = total + matrix[j][i]
+
+    print(total)
