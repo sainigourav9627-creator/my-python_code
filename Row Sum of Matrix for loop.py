9044264a941951f4f9b74ee2b69d6c0949d738bd
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-29 16:49:23 +0530

    Create Row Sum of Matrix for loop.py

---
+++ Row Sum of Matrix for loop.py
@@ -0,0 +1,14 @@
+total=0
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in matrix:
+    total = 0
+    for value in row:
+    
+      total=total+value
+    print(total)
+    
