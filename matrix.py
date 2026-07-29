0336a8cc699a2b6250b6e5d79d0ed27892c8b048
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-29 10:00:37 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -3,7 +3,7 @@ matrix = [
     [4, 5, 6],
     [7, 8, 9]
 ]
-print(matrix)
+
 
           Column
 
@@ -16,4 +16,43 @@ Row 1    4   5   6
 Row 2    7   8   9
 
 
+#row print
+
+print(matrix)
+print(matrix[0])                                                             
+print(matrix[1])
+print(matrix[2])
+
+#matrix[row][column]
+
+print(matrix[0][0])
+print(matrix[0][1])
+print(matrix[0][2])
+
+#matrix[row]
+
+print(matrix[1][0])
+print(matrix[1][1])
+print(matrix[1][2])
+
+#row[column]
+
+print(matrix[2][0])
+print(matrix[2][1])
+print(matrix[2][2])
+
 [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+[1, 2, 3]
+[4, 5, 6]
+[7, 8, 9]
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+=== Code Execution Successful ===
