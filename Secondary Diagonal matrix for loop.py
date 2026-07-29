738d1f9cafeacf750646b69d8abf801735eeed7a
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-29 18:16:02 +0530

    Create Secondary Diagonal matrix for loop.py

---
+++ Secondary Diagonal matrix for loop.py
@@ -0,0 +1,28 @@
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+total = 0
+
+for i in range(3):
+    total = total + matrix[i][2-i]
+
+print(total)
+
+
+first 
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+total = 0
+
+for i in range(3):
+    total = total + matrix[i][i]
+
+print(total)
