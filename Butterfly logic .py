5835232ea6bcb82a50c30bdf7ddbaa425b799736
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 14:44:28 +0530

    Create Butterfly logic .py

---
+++ Butterfly logic .py
@@ -0,0 +1,24 @@
+Easy Logic
+
+Butterfly =
+
+⭐ Left Stars
+⬜ Spaces
+⭐ Right Stars
+
+Upper Half
+
+Stars ➜ badhte hain (1 → rows)
+Spaces ➜ kam hote hain
+
+Lower Half
+
+Stars ➜ ghatte hain (rows-1 → 1)
+Spaces ➜ badhte hain
+⭐ Interview Tip
+
+Butterfly pattern mein 3 inner loops hote hain:
+
+Left Stars
+Spaces
+Right Stars
