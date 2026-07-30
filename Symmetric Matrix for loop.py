c8175b538e9511d5d80f96bb3287e6d663c06707
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 12:01:04 +0530

    Create Symmetric Matrix for loop.py

---
+++ Symmetric Matrix for loop.py
@@ -0,0 +1,17 @@
+matrix = [
+    [1, 2, 3],
+    [2, 4, 5],
+    [3, 5, 6]
+]
+
+flag = True
+
+for i in range(3):
+    for j in range(3):
+        if matrix[i][j] != matrix[j][i]:
+            flag = False
+
+if flag:
+    print("Symmetric Matrix")
+else:
+    print("Not Symmetric Matrix")
