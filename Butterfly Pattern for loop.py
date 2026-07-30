d2dfd9da9ce8becf35e1942e97b0105cd9fda4d8
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 14:43:42 +0530

    Add butterfly pattern printing script
    This script prints a butterfly pattern using nested loops.

---
+++ Butterfly Pattern for loop.py
@@ -0,0 +1,36 @@
+rows = 5
+
+# Upper Half
+for i in range(1, rows + 1):
+
+    # Left Stars
+    for j in range(i):
+        print("*", end="")
+
+    # Spaces
+    for j in range(2 * (rows - i)):
+        print(" ", end="")
+
+    # Right Stars
+    for j in range(i):
+        print("*", end="")
+
+    print()
+    rows = 5
+
+# Lower Half
+for i in range(rows - 1, 0, -1):
+
+    # Left Stars
+    for j in range(i):
+        print("*", end="")
+
+    # Spaces
+    for j in range(2 * (rows - i)):
+        print(" ", end="")
+
+    # Right Stars
+    for j in range(i):
+        print("*", end="")
+
+    print()
