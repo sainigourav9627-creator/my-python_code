b878fed12a437c9dbfab145dfe26969aa3cf4162
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 15:08:39 +0530

    Create Hollow Diamond ⭐Upper Half for loop.py

---
+++ Hollow Diamond ⭐Upper Half for loop.py
@@ -0,0 +1,21 @@
+rows = 5
+
+# Upper Half
+for i in range(rows):
+
+    for j in range(rows - i - 1):
+        print(" ", end="")
+
+    for j in range(2 * i + 1):
+        if j == 0 or j == 2 * i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
+
+    *
+   * *
+  *   *
+ *     *
+*       *
+    
