464d3b8fe3986811c1dd81397af8cf9dd0a15645
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 10:35:00 +0530

    Create matrix addtion 3*3 for loop.py

---
+++ matrix addtion 3*3 for loop.py
@@ -0,0 +1,22 @@
+A = matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+B = matrix = [
+    [1, 6, 3],
+    [4, 7, 6],
+    [8, 0, 9]
+]
+
+for i in range(3):
+    for j in range(3):
+        print(A[i][j]+B[i][j],end=" ")
+    print()
+    
+    
+
+
+    
