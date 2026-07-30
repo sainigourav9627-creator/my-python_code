b00022312753b8b19367be5a6c2bca1e83433602
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 14:48:04 +0530

    Create hollow program conditions.py

---
+++ hollow program conditions.py
@@ -0,0 +1,7 @@
+Yaad rakhne ka Rule
+Pattern	Condition
+
+Hollow Square	i==0 or i==rows-1 or j==0 or j==cols-1
+Hollow Triangle	j==0 or i==j or i==rows-1
+Hollow Pyramid	j==0 or j==2*i or i==rows-1
+X Pattern	i==j or i+j==rows-1
