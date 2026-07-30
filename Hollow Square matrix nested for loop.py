c9b31ed01c7e5845d7235f1c60e995828c407142
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 14:13:39 +0530

    Create Hollow Square matrix nested for loop.py

---
+++ Hollow Square matrix nested for loop.py
@@ -0,0 +1,17 @@
+rows=5
+cols=5
+for i in range(rows):
+    for j in range(cols):
+        if i==0 or i==rows-1 or j==0 or j==cols-1:
+          print("*", end= " ")
+        else:
+          print(" ", end= " ")
+    print()
+
+
+
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
