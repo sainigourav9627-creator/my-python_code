813ee18fe68a2e84065437828cf0c108a6ca57b0
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 15:17:05 +0530

    Create Golden Rules (Interview) pattern .py

---
+++ Golden Rules (Interview) pattern .py
@@ -0,0 +1,12 @@
+Golden Rules (Interview)
+
+i+1 ➜ Triangle
+rows-i ➜ Inverted Triangle
+rows-i-1 ➜ Spaces
+2*i+1 ➜ Pyramid Stars
+2*(rows-i) ➜ Butterfly Spaces
+i==j ➜ Main Diagonal
+i+j==rows-1 ➜ Second Diagonal
+rows//2 ➜ Middle Row/Column
+rows-1 ➜ Last Row
+cols-1 ➜ Last Column	
