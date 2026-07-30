8dd81bebc8672009a8a66ff03fea82f72adfe994
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 10:00:08 +0530

    Simplify row sum calculation using sum()
    Refactored row sum calculation to use built-in sum function.

--- Row Sum of Matrix for loop.py
+++ Row Sum of Matrix for loop.py
@@ -11,4 +11,14 @@ for row in matrix:
     
       total=total+value
     print(total)
-    
+
+or
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in matrix:
+    print(sum(row))
