d24b805b7061b32329114d6b4bb133f17b8e0409
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 14:14:54 +0530

    Create Hollow Rectangle nested for loop.py

---
+++ Hollow Rectangle nested for loop.py
@@ -0,0 +1,15 @@
+rows=4
+cols=8
+for i in range(rows):
+    for j in range(cols):
+        if i==0 or i==rows-1 or j==0 or j==cols-1:
+          print("*", end= " ")
+        else:
+          print(" ", end= " ")
+    print()
+
+
+* * * * * * * * 
+*             * 
+*             * 
+* * * * * * * * 
