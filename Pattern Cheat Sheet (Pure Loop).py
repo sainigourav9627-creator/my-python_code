924f5e043936d5745640191c0d7a77d54be8fd8a
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 15:14:54 +0530

    Create Pattern Cheat Sheet (Pure Loop).py

---
+++ Pattern Cheat Sheet (Pure Loop).py
@@ -0,0 +1,11 @@
+Pattern Cheat Sheet (Pure Loop)
+Pattern	Outer Loop	Inner Loop	Important Formula / Logic
+Square	1	1	range(rows)
+Rectangle	1	1	range(rows) range(cols)
+Hollow Square	1	1	i==0 or i==rows-1 or j==0 or j==cols-1
+Hollow Rectangle	1	1	i==0 or i==rows-1 or j==0 or j==cols-1
+Left Triangle	1	1	range(i+1)
+Inverted Triangle	1	1	range(rows-i)
+Hollow Triangle	1	1	j==0 or i==j or i==rows-1
+X Pattern	1	1	i==j or i+j==rows-1
+Plus (+) Pattern	1	1	i==middle or j==middle
