b54a9f3e3e6770f703abcbb319346d5d31d52eb6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-30 15:11:01 +0530

    Create diamond.py

---
+++ diamond.py
@@ -0,0 +1,6 @@
+Diamond yaad rakhne ka Shortcut
+
+Solid Diamond = Solid Pyramid + Inverted Solid Pyramid
+Hollow Diamond = Hollow Pyramid + Inverted Hollow Pyramid
+
+Isliye agar Pyramid samajh aa gaya, to Diamond bhi aasaan ho jata hai.
