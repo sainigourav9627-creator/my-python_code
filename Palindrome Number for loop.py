056677a862b554b620a275391eaaabfdf8bd6e8b
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-07-31 12:03:56 +0530

    Create Palindrome Number for loop.py

---
+++ Palindrome Number for loop.py
@@ -0,0 +1,24 @@
+original = num → Compare ke liye ✔️
+
+✅ reverse = 0 → Reverse store karne ke liye ✔️
+
+✅ digit = num % 10 → Last digit nikalne ke liye ✔️
+
+✅ reverse = reverse * 10 + digit → Reverse banane ke liye ✔️
+
+✅ num = num // 10 → Last digit hatane ke liye ✔️
+
+✅ if reverse == original → Compare karne ke liye ✔️
+
+⭐ Compare
+num=int(input("enter the armstrong numbe:"))
+original=num
+reverse=0
+while num > 0:
+   digit=num%10
+   reverse=reverse*10+digit
+   num=num//10
+if reverse==original:
+     print("Palindrome Number")
+else:
+       print("NotPalindrome Number")
