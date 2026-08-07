c7faa3505254fe84b227d5223374148f9ad3bee6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-07 14:50:16 +0530

    Create Golden Rule function.py

---
+++ Golden Rule function.py
@@ -0,0 +1,10 @@
+Golden Rule function
+
+Function	                        Input
+
+Simple Function	                 def ke andar input ✅
+Parameter	                       value bahar se do ✅
+Parameter + Return	             value bahar se do + result return karo ✅
+
+Parameter + Return me user input nahi le sakte — ye baat galat hai.
+Le sakte ho, aur tumhara Palindrome wala program iska perfect example hai. ✅
