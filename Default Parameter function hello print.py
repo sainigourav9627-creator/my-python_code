9f7066975b386cce1fca223c335f05b65bf7027f
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-07 15:28:42 +0530

    Create Default Parameter function hello print.py

---
+++ Default Parameter function hello print.py
@@ -0,0 +1,11 @@
+def greet(name="User"):
+    print("Hello", name)
+
+greet()                    hello user
+
+or
+
+def greet(name="User"):
+    print("Hello", name)
+
+greet("Gourav")    hello gourav
