4d9c25a40b17e834ca8d15b45bdcc52feae47ca6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-07 10:26:39 +0530

    Create prime number using function.py

---
+++ prime number using function.py
@@ -0,0 +1,14 @@
+def prime():
+    num = int(input("enter the number:"))
+    count = 0
+
+    for i in range(1, num + 1):
+        if num % i == 0:
+            count = count + 1
+
+    if count == 2:
+        print("prime")
+    else:
+        print("not prime")
+
+prime()
