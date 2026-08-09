eac93ad82e773757f3cad4c20f853ddf9d9094a3
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-09 10:11:26 +0530

    Create **kwargs rule functio.py

---
+++ **kwargs rule functio.py
@@ -0,0 +1,16 @@
+Chaaro ko ek saath compare karo
+Code	Kya milega?
+
+data	            पूरा Dictionary
+data["name"]	    सिर्फ name की value
+data.values()	    सिर्फ सारी values
+data.items()	    key + value दोनों   
+
+Super Memory Trick
+
+Dictionary → अंदर जाना हो:
+
+data              → पूरा डिब्बा 📦
+data["name"]      → एक चीज निकालो
+data.values()     → सारी चीजें
+data.items()      → चीज + उसका नाम
