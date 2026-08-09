68c1856cfa1aad2b2877ef76047aefc8a0a66330
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-09 11:00:59 +0530

    Create **kwargs items function for loop.py

---
+++ **kwargs items function for loop.py
@@ -0,0 +1,11 @@
+def student(**data):
+    for key, value in data.items():
+        print(key, "=", value)
+
+student(name="Gourav", age=25, city="Sambhal", course="Python")
+
+
+name = Gourav
+age = 25
+city = Sambhal
+course = Python
