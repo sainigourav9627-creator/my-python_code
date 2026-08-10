95522d197097894fe26402a6e92371119eea5977
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 10:04:03 +0530

    Create Global variable function.py

---
+++ Global variable function.py
@@ -0,0 +1,16 @@
+Global Variable
+Jo variable function ke bahar banaya jata hai, woh Global Variable hota hai.
+
+name = "Gourav"
+
+def hello():
+    print(name)
+hello()
+
+
+Yahan name → Global Variable hai.
+
+🧠 Easy Trick
+
+Function ke andar → Local
+Function ke bahar → Global
