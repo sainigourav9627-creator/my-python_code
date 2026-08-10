7540187a3d231b33ee6c24b18db7cda2a82a6629
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 14:50:50 +0530

    Update important difference function.py

--- important difference function.py
+++ important difference function.py
@@ -8,3 +8,13 @@ reduce()     →       पूरी list को एक result में combine 
 MAP → Modify
 FILTER → Select
 REDUCE → One Result
+
+
+अब तीनों को एक साथ याद रखो:
+Function	काम
+
+map()	         हर value को बदलना
+filter()	     condition से values चुनना
+reduce()	     सभी values को एक result में बदलना
+
+अब Lambda + map(), filter(), reduce() का basic हिस्सा complete हो गया। ✅
