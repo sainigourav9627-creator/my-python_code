f9ff0395b3e8cba784f62d77debaeb83517bd0ea
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 10:45:05 +0530

    Create Rule Global or Local function.py

---
+++ Rule Global or Local function.py
@@ -0,0 +1,8 @@
+Final rule
+
+Variable	     Function ke andar access    	Function ke bahar access
+Global	      ✅ Haan	                   ✅ Haan
+Local	        ✅ Haan	                    ❌ Nahi
+
+Global = bahar bana → andar + bahar dono jagah use ho sakta hai
+Local = andar bana → sirf andar use ho sakta hai.
