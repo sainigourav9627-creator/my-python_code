066cca88a2a9dafe8b02f491c55a8bc65e33b293
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 14:48:44 +0530

    Create important difference function.py

---
+++ important difference function.py
@@ -0,0 +1,10 @@
+सबसे important difference
+
+map()        →       हर item को बदलता है
+filter()     →       कुछ items चुनता है
+reduce()     →       पूरी list को एक result में combine करता है
+
+याद रखो:
+MAP → Modify
+FILTER → Select
+REDUCE → One Result
