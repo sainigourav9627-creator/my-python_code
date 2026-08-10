78081b13d1a83c106f910ac4458c3a614e247e97
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 15:30:38 +0530

    Update nonlocal function.py

--- nonlocal function.py
+++ nonlocal function.py
@@ -1,6 +1,12 @@
 nonlocal क्या करता है?
 nonlocal का इस्तेमाल nested function में outer function के variable को बदलने के लिए होता है।
 
+याद रखने की Trick
+
+local        → current function का variable
+global       → पूरे program का बाहर वाला variable
+nonlocal    → outer function का variable 🔄
+
 
 def outer():
     x = 10
