7a79911ba1477831b2969b42f27c9e50d1c1f41f
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 16:52:31 +0530

    Create simple yield function.py

---
+++ simple yield function.py
@@ -0,0 +1,11 @@
+def test():
+    yield 10
+    yield 20
+
+याद रखो
+
+yield     → Generator banata hai
+next()     → ek-ek value leta hai
+for       → values automatically leta hai
+
+Toh yield ke liye loop compulsory nahi hai. ✅
