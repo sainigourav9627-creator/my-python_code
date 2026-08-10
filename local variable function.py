b3d7cb9b53be2b53fabdf0ea9a3ebffeb3f00083
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 10:03:16 +0530

    Create local variable function.py

---
+++ local variable function.py
@@ -0,0 +1,12 @@
+Local Variable
+
+Jo variable function ke andar banaya jata hai, woh Local Variable hota hai.
+
+def hello():
+    name = "Gourav"
+    print(name)
+hello()
+
+
+Yahan name →    Local Variable hai.
+Rule:          Local variable ko normally usi function ke andar use karte hain.
