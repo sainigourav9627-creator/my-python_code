edc2e8084d48afd901635c1f43c6efafac8fa28b
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 13:57:10 +0530

    Create lamda function square .py

---
+++ lamda function square .py
@@ -0,0 +1,11 @@
+अब तक Lambda में
+
+lambda →     छोटा function बनाने के लिए
+:            के बाद → expression
+return      लिखने की जरूरत नहीं
+Result automatically return होता है
+
+
+square = lambda num: num * num
+
+print(square(3))
