2a02d8739d623c82a2a75d5518f669e6c4a85001
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-10 16:08:18 +0530

    Update next() वाला Program function.py

--- next() वाला Program function.py
+++ next() वाला Program function.py
@@ -8,3 +8,13 @@ g = numbers()
 print(next(g))
 print(next(g))
 print(next(g))
+
+
+Difference
+
+yield	                 next()
+Function के अंदर	          Function के बाहर
+Value produce करता है	 Value लेता है
+Pause करता है	         Generator को आगे चलाता है
+
+Important: next() generator के साथ ही काम करता है। yield generator बनाने का मुख्य हिस्सा है।
