f3bc38c2076a5adfa3632baabd747854e43e899c
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-11 14:58:37 +0530

    Create Keyword Arguments + Positional Arguments function.py

---
+++ Keyword Arguments + Positional Arguments function.py
@@ -0,0 +1,16 @@
+def student(name, age,city):
+    print(name,age,city)
+student(city="Sambhal", age=32, name="Gourav")
+student("Sambhal",32,"Gourav")
+
+or
+
+def student(name, age,city):
+    print(name,age,city)
+student(city="Sambhal", age=32, name="Gourav")
+
+def student(name, age,city):
+    print(name,age,city)
+student("Sambhal",32,"Gourav")
+
+
