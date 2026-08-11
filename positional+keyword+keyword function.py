db627f94052f4796474b7bb452291243b4afba42
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-11 15:02:57 +0530

    Create positional+keyword+keyword function.py

---
+++ positional+keyword+keyword function.py
@@ -0,0 +1,13 @@
+def student(name, age,city):
+    print(name,age,city)
+student("Gourav",city="Sambhal", age=32,)
+
+"Gourav" → Positional Argument ✅
+city="Sambhal" → Keyword Argument ✅
+age=32 → Keyword Argument ✅
+
+
+Rule yaad rakho ⭐
+
+Positional → pehle
+Keyword → baad mein
