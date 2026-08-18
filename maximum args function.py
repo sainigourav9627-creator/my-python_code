459263582db3c4c407d9d26cf1ebda22a5987a3f
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-18 16:20:57 +0530

    Update maximum args function.py

--- maximum args function.py
+++ maximum args function.py
@@ -9,4 +9,20 @@ def maximum(*args):
 
 result = maximum(10, 20, 30)
 print(result)
+
+
+or
+
+def maximum(*args):
+    maximum = args[0]
+
+    for num in args:
+        if num > maximum:
+            maximum = num
+
+    return maximum
+
+result = maximum(10, 25, 7, 40, 15)
+
+print(result)
  
