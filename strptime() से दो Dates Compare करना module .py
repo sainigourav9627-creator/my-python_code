118084a8d0ab3c71343c02bb919180f396b1b5f5
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-25 11:57:08 +0530

    Update strptime() से दो Dates Compare करना module .py

--- strptime() से दो Dates Compare करना module .py
+++ strptime() से दो Dates Compare करना module .py
@@ -6,6 +6,6 @@ date2 = datetime.strptime("30-08-2026", "%d-%m-%Y")
 if date1 > date2:
     print("Date 1 is greater")
 elif date1 < date2:
-    print("Date 2 is greatrer")
+    print("Date 2 is greater")
 else:
     print("both date are equal")
