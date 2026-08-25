99de4c34a9911fa27f9dedd586c80fc565d9cb52
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-25 11:34:04 +0530

    Update केवल Time को strptime() से Convert करना module .py

--- केवल Time को strptime() से Convert करना module .py
+++ केवल Time को strptime() से Convert करना module .py
@@ -3,3 +3,14 @@ from datetime import datetime
 date = datetime.strptime("14:30:25", "%H:%M:%S")
 
 print(date)
+
+
+
+अब तक के important defaults
+Date only
+25-08-2026
+→ 2026-08-25 00:00:00
+
+Time only
+14:30:25
+→ 1900-01-01 14:30:25
