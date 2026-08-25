f815ebd4d1c74ccb092e2773e4c83ed892932dd5
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-25 10:13:55 +0530

    Create datetime + weekday() + strftime() + strptime()  syntax module .py

---
+++ datetime + weekday() + strftime() + strptime()  syntax module .py
@@ -0,0 +1,28 @@
+Topic	Main Syntax
+
+datetime	            datetime.now()
+weekday()	            date.weekday()
+strftime()	          datetime.strftime("format")
+strptime()	          datetime.strptime("date_string", "format")
+
+
+
+
+Conversion yaad rakho
+
+
+datetime.now()
+      ↓
+Current Date + Time
+
+weekday()
+      ↓
+Day Number
+
+strftime()
+      ↓
+datetime → String
+
+strptime()
+      ↓
+String → datetime
