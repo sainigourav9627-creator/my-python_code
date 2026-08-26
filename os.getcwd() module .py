8a91a680a10e3d93be952ff2574d48b8eb8d5af7
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-26 10:05:00 +0530

    Create os.getcwd() module .py

---
+++ os.getcwd() module .py
@@ -0,0 +1,36 @@
+Topic 1: import os
+
+os Python ka built-in module hai jo operating system ke saath kaam karne ke liye use hota hai.
+
+  Isse hum:
+
+files/folders dekh sakte hain
+folder create/delete kar sakte hain
+current location pata kar sakte hain
+files/folders rename kar sakte hain
+paths ke saath kaam kar sakte hain
+
+  Syntax
+
+  import os
+
+  import os
+
+print(os.getcwd())
+
+
+  Get Current Working Directory
+
+Ye batata hai ki Python abhi kis folder/location mein kaam kar raha hai.
+
+
+  Yaad rakho
+
+  
+  import os →    OS module ko import karta hai
+os.getcwd() →    Current Working Directory batata hai
+getcwd =        get current working directory
+
+
+
+  
