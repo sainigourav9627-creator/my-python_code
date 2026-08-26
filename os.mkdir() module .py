325b730224c51ab19d7060aa7ee1ae2a91a67654
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-26 10:23:41 +0530

    Create os.mkdir() module .py

---
+++ os.mkdir() module .py
@@ -0,0 +1,19 @@
+os.mkdir()
+os.mkdir() ka use naya folder create karne ke liye hota ha
+
+
+import os
+os.mkdir("Python")
+
+
+
+Check karne ke liye
+
+import os
+os.mkdir("Python")
+print(os.listdir())
+
+
+Output mein Python folder dikhega:
+
+['Python', ...]
