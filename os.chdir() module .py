ec4bd56b1f79836526419a3db68d740fc4cfa2e6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-26 10:13:21 +0530

    Create os.chdir() module .py

---
+++ os.chdir() module .py
@@ -0,0 +1,24 @@
+chdir() = Change Directory
+
+Iska use current working directory ko change karne ke liye hota hai.
+
+Syntax:
+os.chdir("folder_path")
+
+import os
+
+os.chdir("C:\\Users\\Gourav\\Documents")
+
+print(os.getcwd())
+
+
+
+Practice
+
+Aap apne laptop par ek folder ka path dekar ye program likho:
+
+import os
+
+os.chdir("________")
+print(os.getcwd())
+
