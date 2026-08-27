cada8f2169b23cc0a0210abbfad5dafb7a962b5f
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-27 11:13:22 +0530

    Create Folder का नाम बदलना module .py

---
+++ os.rename() — File/Folder का नाम बदलना module .py
@@ -0,0 +1,14 @@
+import os
+
+os.rename("old_name", "new_name")
+
+
+
+import os
+
+os.rename(
+    r"C:\Users\ajski\OneDrive\Desktop\Python_Practice\test.txt",
+    r"C:\Users\ajski\OneDrive\Desktop\Python_Practice\new_test.txt"
+)
+
+print("File renamed")
