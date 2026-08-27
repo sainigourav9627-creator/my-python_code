ea2c25aae14769afe0bfc0b2349b56a7531fd480
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-27 11:23:07 +0530

    Create os.path.isdir() — Folder है या नहीं? os module .py

---
+++ os.path.isdir() — Folder है या नहीं? os module .py
@@ -0,0 +1,14 @@
+os.path.isdir() का use check करने के लिए होता है कि दिया गया path directory/folder है या नहीं।
+
+
+Syntax
+
+import os
+os.path.isdir("path")
+
+
+
+यह भी:
+
+True  → Folder है
+False → Folder नहीं है
