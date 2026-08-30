ba39711ff0f3b637a34f183fe342f4dc7652631f
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-30 14:36:26 +0530

    Create os.path.abspath() module .py

---
+++ os.path.abspath() module .py
@@ -0,0 +1,27 @@
+os.path.abspath()
+
+os.path.abspath() का use किसी path का absolute/full path निकालने के लिए होता है।
+
+
+Syntax
+os.path.abspath(path)
+
+
+import os
+
+path = "Python_Practice"
+
+print(os.path.abspath(path))
+
+
+
+
+C:\Users\ajski\AppData\Local\Programs\Microsoft VS Code
+
+
+C:\Users\ajski\AppData\Local\Programs\Microsoft VS Code\Python_Practice
+
+
+यानी relative path:
+
+Python_Practice
