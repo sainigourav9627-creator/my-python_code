d2a7df2b556cb26a294137a5bd394595b28b18b8
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-31 14:00:22 +0530

    Create sys.maxsize modue .py

---
+++ sys.maxsize modue .py
@@ -0,0 +1,14 @@
+sys.maxsize शुरू करते हैं।
+
+🔹 sys.maxsize
+
+sys.maxsize एक बहुत बड़ा integer value देता है, जो Python के platform पर Py_ssize_t type की maximum value को represent करता है।
+
+
+
+import sys
+
+print(sys.maxsize)
+
+x = sys.maxsize + 100
+print(x)
