25c7b787cf1ad5025ea231c738e359aa2c75ecce
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-31 13:50:05 +0530

    Create sys .module .py

---
+++ sys .module .py
@@ -0,0 +1,17 @@
+sys = System
+
+Python ka sys module hume Python interpreter aur system ke saath interact karne ke tools deta hai.
+
+
+Topic 1: sys kya hai?
+
+sys Python ka built-in module hai.
+
+Isko use karne ke liye:
+
+import sys
+
+
+import sys
+
+print(sys.version)
