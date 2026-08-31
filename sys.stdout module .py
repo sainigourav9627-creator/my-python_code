7a675c73448493ec9b547d0b33412bd800096795
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-08-31 14:16:07 +0530

    Update sys.stdout module .py

--- sys.stdout module .py
+++ sys.stdout module .py
@@ -4,9 +4,8 @@ sys.stdout का मतलब है Standard Output.
 
 हम normally output दिखाने के लिए:
 
-
 import sys
 
 sys.stdout.write("Hello ")
 sys.stdout.write("Gourav")
-
+sys.stdout.write("\nWelcome to Python")
