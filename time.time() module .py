9b3c767cbadca74d93a73370f8014dfd436e95de
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:43:55 +0530

    Update time.time() module .py

--- time.time() module .py
+++ time.time() module .py
@@ -3,3 +3,18 @@ import time
 result = time.time()
 
 print(result)
+
+
+or
+
+
+import time
+
+start = time.time()
+
+for i in range(1000000):
+    pass
+
+end = time.time()
+
+print("Time taken:", end - start)
