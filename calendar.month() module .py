f96e16b2467bc5df2411db03be3f48f8dd1c4161
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:17:51 +0530

    Create calendar.month() module .py

---
+++ calendar.month() module .py
@@ -0,0 +1,25 @@
+import calendar
+
+print(calendar.month(2026, 9))
+
+
+
+
+2026 → Year
+9    → September (Month)
+
+
+
+
+Output लगभग ऐसा होगा:
+
+  
+
+
+September 2026
+Mo Tu We Th Fr Sa Su
+    1  2  3  4  5  6
+ 7  8  9 10 11 12 13
+14 15 16 17 18 19 20
+21 22 23 24 25 26 27
+28 29 30
