4fdcf9627ae57f24fd885856e0c15cad58520e9b
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 09:57:09 +0530

    Create statistics Module .py

---
+++ statistics Module .py
@@ -0,0 +1,20 @@
+statistics Module
+
+statistics Python का built-in module है, जिसका उपयोग numerical data का statistical calculation करने के लिए होता है।
+
+
+
+
+सबसे important functions
+
+| Function     | काम                            |
+| ------------ | ------------------------------ |
+| `mean()`     | Average निकालना                |
+| `median()`   | Middle value                   |
+| `mode()`     | सबसे ज्यादा बार आने वाली value |
+| `stdev()`    | Standard deviation             |
+| `variance()` | Variance                       |
+
+
+
+
