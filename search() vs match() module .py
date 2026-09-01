eb0d08865e8d67c062f856e7919fe955bab7fdfe
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 15:25:14 +0530

    Create search() vs match() module .py

---
+++ search() vs match() module .py
@@ -0,0 +1,15 @@
+re.search()
+→ पूरे string में कहीं भी खोजता है
+
+re.match()
+→ केवल शुरुआत से check करता है
+
+
+
+"I love Python"
+ ↑
+ match() → Python नहीं मिलेगा
+
+"I love Python"
+         ↑
+ search() → Python मिल जाएगा
