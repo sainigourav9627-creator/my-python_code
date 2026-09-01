4cd568ebc16e222ce04ceb7234d081fdfab57d1d
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:22:34 +0530

    Create calendar.monthrange() module .py

---
+++ calendar.monthrange() module .py
@@ -0,0 +1,23 @@
+import calendar
+
+print(calendar.monthrange(2026, 9))
+
+
+
+Output:
+
+(1, 30)
+
+
+
+
+यहाँ:
+
+1  → Month का पहला दिन किस weekday पर है
+30 → उस month में कुल कितने days हैं
+
+
+यानी September 2026 में:
+
+पहला दिन → Tuesday (1)
+कुल days → 30
