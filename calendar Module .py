0ed1c38635a2003dbc0fb56a8095f7277754d9e7
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:41:27 +0530

    Update calendar Module .py

--- calendar Module .py
+++ calendar Module .py
@@ -6,3 +6,26 @@ calendar Python का built-in module है। इसका इस्तेम
 
 
 import calendar
+
+
+
+| Function       | काम                        | Syntax                         |
+| -------------- | -------------------------- | ------------------------------ |
+| `isleap()`     | Leap year check            | `calendar.isleap(2024)`        |
+| `month()`      | एक महीने का calendar       | `calendar.month(2026, 9)`      |
+| `calendar()`   | पूरे साल का calendar       | `calendar.calendar(2026)`      |
+| `weekday()`    | Weekday number             | `calendar.weekday(2026, 9, 1)` |
+| `monthrange()` | First weekday + total days | `calendar.monthrange(2026, 9)` |
+
+
+
+
+weekday() Number
+
+0 → Monday
+1 → Tuesday
+2 → Wednesday
+3 → Thursday
+4 → Friday
+5 → Saturday
+6 → Sunday
