abf4cbb4af5df3363be7906580ef2d60ef55edfe
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:20:02 +0530

    Create calendar.weekday() module .py

---
+++ calendar.weekday() module .py
@@ -0,0 +1,14 @@
+import calendar
+
+print(calendar.weekday(2026, 9, 1))
+
+
+यह 0 से 6 के बीच number देगा।
+
+0 → Monday
+1 → Tuesday
+2 → Wednesday
+3 → Thursday
+4 → Friday
+5 → Saturday
+6 → Sunday
