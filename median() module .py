e48008abd9e374baebd090b71d5a927a0864f224
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 09:59:48 +0530

    Update median() module .py

--- median() module .py
+++ median() module .py
@@ -3,3 +3,20 @@ import statistics
 data = [10, 20, 30, 40, 50]
 
 print(statistics.median(data))
+
+
+import statistics
+
+data = [10, 20, 30, 40]
+
+print(statistics.median(data))
+
+
+
+बीच की दो values:
+
+20 और 30
+
+इनका average:
+
+(20 + 30) / 2 = 25
