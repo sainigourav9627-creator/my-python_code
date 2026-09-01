be7c304a3bdc1b56f956ec3093c82fb5047f3aaa
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:48:18 +0530

    Create time.sleep() ⭐ module .py

---
+++ time.sleep() ⭐ module .py
@@ -0,0 +1,29 @@
+time.sleep() ⭐
+
+यह बहुत important function है।
+
+time.sleep(seconds) → program को दिए गए seconds तक रोक देता है।
+
+
+
+import time
+
+print("Hello")
+
+time.sleep(3)
+
+print("Welcome")
+
+
+
+
+Output में:
+
+Hello
+
+
+
+
+के बाद 3 seconds का इंतजार होगा, फिर:
+
+Welcome
