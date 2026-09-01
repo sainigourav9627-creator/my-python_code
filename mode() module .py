bfb9679e0a243ce02e89bdfc593ece6465d8bf56
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-01 10:01:04 +0530

    Update mode() module .py

--- mode() module .py
+++ mode() module .py
@@ -3,3 +3,21 @@ import statistics
 data = [10, 20, 20, 30, 40]
 
 print(statistics.mode(data))
+
+
+or
+
+
+
+import statistics
+
+data = [5, 10, 15, 20, 25]
+
+print(statistics.mean(data))
+print(statistics.median(data))
+print(statistics.mode(data))
+
+
+इस data में सभी values सिर्फ एक-एक बार आई हैं:
+
+5, 10, 15, 20, 25
