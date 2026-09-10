09046c0ee874b3084c7a7f6df141fa0011ec6c5e
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 14:25:09 +0530

    Create पूरा File एक String में पढ़ना syntax file handling .py

---
+++ पूरा File एक String में पढ़ना syntax file handling .py
@@ -0,0 +1,18 @@
+with open("student.txt", "r") as file:
+    data = file.read()
+
+
+
+Word Search
+Word Count
+Character Count
+Replace Word
+Remove Word
+Total Words
+Character Frequency
+Digit Count
+Alphabet Count
+Special Character Count
+Most Frequent Word
+Final Mixed Program
+
