2d9334168bb2c15f85837ff1390c6050b353db85
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 11:10:31 +0530

    Update x Mode — Create Mode file handling .py

--- x Mode — Create Mode file handling .py
+++ x Mode — Create Mode file handling .py
@@ -20,3 +20,7 @@ file.close()
 ⚠️ अगर उसी नाम की file पहले से मौजूद है, तो x mode error देगा।
 
 इसे अभी practical में करेंगे, ताकि w और x का difference भी clear हो जाए।
+
+
+w → file मौजूद हो तो पुराना data overwrite कर सकता है
+x → file मौजूद हो तो error, नई file ही create करेगा
