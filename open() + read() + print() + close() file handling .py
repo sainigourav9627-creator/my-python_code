6fd9fbc6577c27de3158d0312b332d8ca422b5e6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 10:47:44 +0530

    Create open() + read() + print() + close() file handling .py

---
+++ open() + read() + print() + close() file handling .py
@@ -0,0 +1,25 @@
+file = open("student.txt")
+
+data = file.read()
+
+print(data)
+
+file.close()
+
+
+और student.txt के अंदर:
+
+My name is Gourav.
+I am learning Python.
+
+
+Output
+My name is Gourav.
+I am learning Python.
+
+
+⭐ अभी तक का Formula
+open()  → file खोलना
+read()  → data पढ़ना
+print() → data दिखाना
+close() → file बंद करना
