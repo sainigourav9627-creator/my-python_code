49ba597418bcba41c39f860ff72aa5756d523570
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 11:41:38 +0530

    Create with open() पूरा chart .py

---
+++ with open() पूरा chart .py
@@ -0,0 +1,6 @@
+| Code                         | काम               |
+| ---------------------------- | ----------------- |
+| `with open("file.txt", "r")` | Read              |
+| `with open("file.txt", "w")` | Overwrite + Write |
+| `with open("file.txt", "a")` | Append            |
+| `with open("file.txt", "x")` | New file Create   |
