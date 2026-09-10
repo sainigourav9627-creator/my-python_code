b7a8fd86b990b1ccd921d2e319e748ef745d66e4
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 14:26:17 +0530

    Create Template 3 — File पढ़ो → Data बदलो → वापस Save करो file handling .py

---
+++ Template 3 — File पढ़ो → Data बदलो → वापस Save करो file handling .py
@@ -0,0 +1,13 @@
+with open("student.txt", "r") as file:
+    data = file.read()
+
+# Logic यहाँ
+
+with open("student.txt", "w") as file:
+    file.write(data)
+
+
+Python → Java
+Java → हटाना
+Python → Programming
+Programming → Python
