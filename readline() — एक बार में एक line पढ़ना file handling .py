3df30250eaa3679daf7f300e698f9d196b296e05
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 11:19:06 +0530

    Create readline() — एक बार में एक line पढ़ना file handling .py

---
+++ readline() — एक बार में एक line पढ़ना file handling .py
@@ -0,0 +1,17 @@
+file = open("student.txt", "r")
+
+line = file.readline()
+
+print(line)
+
+file.close()
+
+
+My name is Gourav.
+I am learning Python.
+I am learning File Handling.
+
+
+
+Output
+My name is Gourav.
