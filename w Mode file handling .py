199cc7ab494374f10af9603c4328f2b46da6a6a6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 10:56:48 +0530

    Create w Mode file handling .py

---
+++ w Mode file handling .py
@@ -0,0 +1,24 @@
+file = open("student.txt", "w")
+
+
+file = open("student.txt", "w")
+
+file.write("Python File Handling")
+
+file.close()
+
+
+w mode पुराना content मिटाकर नया content लिखता है।
+
+
+
+My name is Gourav.
+I am learning Python.
+
+
+after
+
+  Python File Handling
+
+
+  
