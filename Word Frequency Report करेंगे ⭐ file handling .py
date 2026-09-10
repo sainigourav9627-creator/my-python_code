e6d52d302e8a0c6429a1420544aa4f760da12129
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 14:00:57 +0530

    Create Word Frequency Report करेंगे ⭐ file handling .py

---
+++ Word Frequency Report करेंगे ⭐ file handling .py
@@ -0,0 +1,17 @@
+with open("student.txt", "r") as file:
+    data = file.read()
+
+words = data.split()
+
+count = {}
+
+for word in words:
+    word = word.strip(".,!?")
+
+    if word in count:
+        count[word] += 1
+    else:
+        count[word] = 1
+
+for word, frequency in count.items():
+    print(word, "→", frequency)
