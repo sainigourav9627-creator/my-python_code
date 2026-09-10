0e27e29deabd62805b3a73d1d719d74c77735bfe
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 14:25:42 +0530

    Create पूरी File की Lines पढ़ना syntax file handling .py

---
+++ पूरी File की Lines पढ़ना syntax file handling .py
@@ -0,0 +1,13 @@
+with open("student.txt", "r") as file:
+    lines = file.readlines()
+
+Line Count
+Particular Line
+Print All Lines
+Longest Line
+Shortest Line
+Word in Which Line
+Number of Lines Containing Word
+Reverse Lines
+Sort Lines
+Blank Lines Remove
