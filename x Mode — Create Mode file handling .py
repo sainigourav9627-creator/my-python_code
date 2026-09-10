2b0a3816a9132a30168900fc110f2780eb027403
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 11:10:03 +0530

    Create x Mode — Create Mode file handling .py

---
+++ x Mode — Create Mode file handling .py
@@ -0,0 +1,22 @@
+file = open("newfile.txt", "x")
+
+
+file = open("newfile.txt", "x")
+
+file.write("This is my new file.")
+
+file.close()
+
+
+
+
+
+
+
+
+
+नई file create करना।
+
+⚠️ अगर उसी नाम की file पहले से मौजूद है, तो x mode error देगा।
+
+इसे अभी practical में करेंगे, ताकि w और x का difference भी clear हो जाए।
