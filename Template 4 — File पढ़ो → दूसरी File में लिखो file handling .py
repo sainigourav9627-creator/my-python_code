769fadb62456b947e501a71640abc9fd9ae7fbb9
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-10 14:26:59 +0530

    Create Template 4 — File पढ़ो → दूसरी File में लिखो file handling .py

---
+++ Template 4 — File पढ़ो → दूसरी File में लिखो file handling .py
@@ -0,0 +1,10 @@
+with open("student.txt", "r") as source:
+    data = source.read()
+
+with open("new_file.txt", "w") as destination:
+    destination.write(data)
+
+
+File Copy
+File Combine
+Clean File
