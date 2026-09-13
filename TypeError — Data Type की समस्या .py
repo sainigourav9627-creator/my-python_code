21b49d0477ba3df08e87f4d0fb93e18017eea3a6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-13 11:30:06 +0530

    Update TypeError — Data Type की समस्या .py

--- TypeError — Data Type की समस्या .py
+++ TypeError — Data Type की समस्या .py
@@ -1,14 +1,7 @@
-TypeError — Data Type की समस्या
-
-जब हम गलत data types को ऐसे operation में इस्तेमाल करते हैं जहाँ वे compatible नहीं हैं।
-
-
 age = 20
 
 print(age + " years")
 
-
-यहाँ:
 
 int + str
 Python इन्हें सीधे जोड़ नहीं सकता।
