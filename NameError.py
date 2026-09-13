fa8af7bf3d77307a36cee2a6acf78ecad477e515
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-13 11:26:21 +0530

    Document NameError in NameError.py
    Added explanation and example for NameError in Python.

---
+++ NameError.py
@@ -0,0 +1,15 @@
+NameError — नाम नहीं मिला
+जब हम किसी ऐसे variable/function का नाम इस्तेमाल करते हैं जो बनाया ही नहीं गया।
+
+
+
+name = "Gourav"
+
+print(age)
+
+
+हमने age बनाया ही नहीं।
+
+
+Output:
+NameError: name 'age' is not defined
