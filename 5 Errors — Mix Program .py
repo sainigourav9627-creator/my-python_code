338a78b63b3a7af79d7137cacde68e10a1de4cff
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-14 14:47:23 +0530

    Create 5 Errors — Mix Program .py

---
+++ 5 Errors — Mix Program .py
@@ -0,0 +1,18 @@
+# 1. SyntaxError
+print("Hello Python"
+
+# 2. NameError
+print(u)
+
+# 3. TypeError
+u = "10"
+x = 5
+print(u + x)
+
+# 4. ValueError
+print(int("hello"))
+
+# 5. ZeroDivisionError
+a = 10
+b = 0
+print(a / b)
