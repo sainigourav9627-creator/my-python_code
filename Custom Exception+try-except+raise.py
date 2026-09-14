33ed5b5176dd978bb1c1e084a3d48ea146f0e523
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-14 14:20:14 +0530

    Create Custom Exception+try-except+raise.py

---
+++ Custom Exception+try-except+raise.py
@@ -0,0 +1,14 @@
+class InvalidMarksError(Exception):
+    pass
+
+
+try:
+    marks = int(input("Enter marks: "))
+
+    if marks < 0 or marks > 100:
+        raise InvalidMarksError("Marks 0 se 100 ke beech hone chahiye.")
+
+    print("Valid marks:", marks)
+
+except InvalidMarksError as e:
+    print("Error:", e)
