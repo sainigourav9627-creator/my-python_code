49927d12d947a4c6f6b9398beb0b9375cdcd92f3
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-17 11:11:34 +0530

    Create Method oops.py

---
+++ Method oops.py
@@ -0,0 +1,22 @@
+class Student:
+
+    def study(self):
+        print("Student is studying")
+
+
+study() → Method
+
+s1 = Student()
+
+s1.study()
+
+
+class Student:
+
+    def study(self):
+        print("Student is studying")
+
+
+s1 = Student()
+
+s1.study()
