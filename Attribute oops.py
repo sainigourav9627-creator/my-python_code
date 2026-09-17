1d60672c9ed5f2418f7d9e6bc1ef36866f06f7d1
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-17 11:10:10 +0530

    Create Attribute oops.py

---
+++ Attribute oops.py
@@ -0,0 +1,22 @@
+class Student:
+    pass
+
+s1 = Student()
+
+s1.name = "Gourav"
+s1.age = 20
+
+name → Attribute
+age → Attribute
+
+class Student():
+  pass
+  
+s1=Student()
+
+s1.name = "Gourav"
+s1.age = 20
+s1.course ="java"
+print(s1.name)
+print(s1.age)
+print(s1.course)
