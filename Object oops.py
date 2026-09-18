aa5f3d709f7511370742180a90738c59c64ae5ec
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 15:01:51 +0530

    Update Object oops.py

--- Object oops.py
+++ Object oops.py
@@ -3,20 +3,4 @@ class Student:
 
 s1 = Student()
 
-
-Student → Class
-s1 → Object
-
-
-
-class Student():
-  pass
-  
-s1=Student()
-
-s1.name = "Gourav"
-s1.age = 20
-s1.course ="java"
-print(s1.name)
-print(s1.age)
-print(s1.course)
+print(s1)
