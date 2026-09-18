2c339f4af2ce1a8e3da66fb7a18226c7fd1652d2
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 15:12:41 +0530

    Create 2 Objects oops .py

---
+++ 2 Objects oops .py
@@ -0,0 +1,16 @@
+class Student:
+    pass
+
+s1 = Student()
+s1.name = "Gourav"
+s1.age = 25
+
+s2 = Student()
+s2.name = "Amit"
+s2.age = 30
+
+print(s1.name)
+print(s1.age)
+
+print(s2.name)
+print(s2.age)
