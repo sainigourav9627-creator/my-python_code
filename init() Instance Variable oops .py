64158402abb385aa173b2e3a31c330ea2ce4095a
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 14:38:59 +0530

    Create init() Instance Variable oops .py

---
+++ init() Instance Variable oops .py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, course):
+        self.name = name
+        self.course = course
+
+s1 = Student("Gourav", "Python")
+s2 = Student("Amit", "Java")
+
+print(s1.name)
+print(s2.name)
+print(s1.course)
+print(s2.course)
