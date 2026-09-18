bf9ea570884d5e7c9b6820ed0e0cfda014c073ab
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 11:47:26 +0530

    Create __init__() में 2 Parameters oops .py

---
+++ __init__() में 2 Parameters oops .py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+s1 = Student("anil", 25)
+s2 = Student("amit", 30)
+
+print(s1.name)
+print(s1.age)
+
+print(s2.name)
+print(s2.age)
