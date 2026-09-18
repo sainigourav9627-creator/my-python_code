cd156352be61c9945a8cef2625c68909c27084b2
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 15:00:40 +0530

    Update Class oops.py

--- Class oops.py
+++ Class oops.py
@@ -1,19 +1,4 @@
 class Student:
     pass
-  
-यहाँ Student = Class है।
-Class सिर्फ design है।
 
-
-
-class Student():
-  pass
-  
-s1=Student()
-
-s1.name = "Gourav"
-s1.age = 20
-s1.course ="java"
-print(s1.name)
-print(s1.age)
-print(s1.course)
+print("Student class created")
