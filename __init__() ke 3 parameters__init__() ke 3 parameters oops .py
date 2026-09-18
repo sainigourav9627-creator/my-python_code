b0f2fbc9cad9c24598f5e39ea9d9d259bbd7e261
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 14:16:57 +0530

    Create __init__() ke 3 parameters__init__() ke 3 parameters oops .py

---
+++ __init__() ke 3 parameters__init__() ke 3 parameters oops .py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age, course):
+        self.name = name
+        self.age = age
+        self.course = course
+
+s1 = Student("Anil", 25, "Python")
+
+print(s1.name)
+print(s1.age)
+print(s1.course)
