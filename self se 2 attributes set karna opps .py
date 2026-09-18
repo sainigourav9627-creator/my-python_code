035feef6f9f8f0a56c74ac374570f6f5792114f3
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-18 17:35:18 +0530

    Create self se 2 attributes set karna opps .py

---
+++ self se 2 attributes set karna opps .py
@@ -0,0 +1,15 @@
+class Student:
+
+    def set_data(self):
+        self.name = "Gourav"
+        self.age = 25
+
+    def show(self):
+        print(self.name)
+        print(self.age)
+
+
+s1 = Student()
+
+s1.set_data()
+s1.show()
