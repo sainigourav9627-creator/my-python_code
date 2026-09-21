d01fd9784b43bf7f64dd8b891e50b61bab2b1d3f
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-21 10:06:31 +0530

    Create Encapsulation oops .py

---
+++ Encapsulation oops .py
@@ -0,0 +1,17 @@
+Encapsulation = Data ko control ke saath class ke andar rakhna.
+
+Sirf data ko class ke andar rakhna hi main point nahi hai. Us data ko kaise access/change kiya jaaye, us par control Encapsulation ka important part hai.
+
+
+Balance = ₹50,000
+
+balance = -₹10,000
+
+Deposit → valid amount
+Withdraw → balance se zyada nahi
+
+Data
+  +
+Controlled Access
+  ↓
+Encapsulation
