43e10aabf35966692759a626d6b360f3127e9c41
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-21 14:53:34 +0530

    Create Encapsulation @property Getter mein oops .py

---
+++ Encapsulation @property Getter mein oops .py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, marks):
+        self.__marks = marks
+
+    @property
+    def marks(self):
+        return self.__marks
+
+s1 = Student(80)
+
+print(s1.marks)
