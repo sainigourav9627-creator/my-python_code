bdb75c601490c666c105984f069dd9094c3175da
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-21 13:50:46 +0530

    Update Encapsulation — Step 6: Public Variable oops .py

--- Encapsulation — Step 6: Public Variable oops .py
+++ Encapsulation — Step 6: Public Variable oops .py
@@ -9,18 +9,3 @@ print(s1.name)
 
 
 
-2️⃣ Bahar se direct change
-
-
-class Student:
-    def __init__(self):
-        self.name = "Gourav"
-
-
-s1 = Student()
-
-print(s1.name)
-
-s1.name = "Amit"
-
-print(s1.name)
