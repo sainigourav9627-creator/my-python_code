47c30ef8d3be124fb135e0b624b3a7be9fe9e4d6
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-21 13:48:18 +0530

    Allow modification of student name after creation
    Added functionality to change the student's name after instantiation.

---
+++ Encapsulation — Step 6: Public Variable oops .py
@@ -0,0 +1,26 @@
+class Student:
+    def __init__(self):
+        self.name = "Gourav"
+
+
+s1 = Student()
+
+print(s1.name)
+
+
+
+2️⃣ Bahar se direct change
+
+
+class Student:
+    def __init__(self):
+        self.name = "Gourav"
+
+
+s1 = Student()
+
+print(s1.name)
+
+s1.name = "Amit"
+
+print(s1.name)
