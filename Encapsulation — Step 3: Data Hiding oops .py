5a50cd063b8fdeb2a6ef5bb747037104cc01ccaf
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-21 10:11:13 +0530

    Create Encapsulation — Step 3: Data Hiding oops .py

---
+++ Encapsulation — Step 3: Data Hiding oops .py
@@ -0,0 +1,20 @@
+Data Hiding ka simple meaning hai:
+
+Class ke important/sensitive data ko bahar se direct access ya modification se protect karna.
+
+
+  
+  Important Difference
+
+
+Encapsulation = data + controlled access
+
+Data Hiding = data ko direct access se protect karna
+
+Encapsulation
+     ↓
+Data + Methods
+     ↓
+Controlled Access
+     ↓
+Data Hiding
