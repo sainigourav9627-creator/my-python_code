b532e6f655eaee24d0281531075b4b75cd7e47f4
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-23 15:16:42 +0530

    Create MRO में super() Inheritance .py

---
+++ MRO में super() Inheritance .py
@@ -0,0 +1,15 @@
+class Parent1:
+    def show(self):
+        print("Parent 1")
+
+class Parent2:
+    def show(self):
+        print("Parent 2")
+
+class Child(Parent1, Parent2):
+    def show(self):
+        super().show()
+        print("Child")
+
+c = Child()
+c.show()
