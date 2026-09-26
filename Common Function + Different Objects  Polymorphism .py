a8868baad69053136abbbae2c8e88a0b3bb12bbc
sainigourav9627-creator <sainigourav9627@gmail.com>
2026-09-26 10:40:18 +0530

    Create Common Function + Different Objects  Polymorphism .py

---
+++ Common Function + Different Objects  Polymorphism .py
@@ -0,0 +1,19 @@
+class Dog:
+    def sound(self):
+        print("Bark")
+
+
+class Cat:
+    def sound(self):
+        print("Meow")
+
+
+def make_sound(animal):
+    animal.sound()
+
+
+d = Dog()
+c = Cat()
+
+make_sound(d)
+make_sound(c)
